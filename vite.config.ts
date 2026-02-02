620bd60071800b560140e1970b6399d489517234
Eyejangg <ptwptw1550@gmail.com>
2026-02-02 14:45:49 +0700

    ViteConfig-

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+
+
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/react-app-66-Git/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
